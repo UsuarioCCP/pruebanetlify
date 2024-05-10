8c204603abe9cceda4809451d2cb0560d9932d7c
archivoccp@gmail.com <archivoccp@gmail.com>
2024-05-10 11:31:14 -0500

    se crea componente y ruta notfound para manejar pagina no encontrada

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ImagenesProvider } from './componentes/Context/imagenContext';
 import Home from './pages/home';
 import Admin from './pages/Admin';
 import Navbar from './componentes/Navbar';
+import NotFound from './componentes/NotFound';
 // import About from './components/About';
 // import NotFound from './components/NotFound';
 
@@ -16,7 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/admin" element={<Admin />} />
-            {/* <Route path="*" element={<NotFound />} /> */}
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </BrowserRouter>
