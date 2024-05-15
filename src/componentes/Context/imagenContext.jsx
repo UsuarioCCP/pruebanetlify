a7b278651dd50b88c764d97db719a4ab67c8383f
archivoccp@gmail.com <archivoccp@gmail.com>
2024-05-15 15:40:54 -0500

    se agrego nuevo elemento de inicio para ser reproducido en el carousel

--- src/componentes/Context/imagenContext.jsx
+++ src/componentes/Context/imagenContext.jsx
@@ -10,6 +10,7 @@ export const ImagenesProvider = ({ children }) => {
     { url: "https://i.postimg.cc/kgBDFXGD/441533923-462431496158834-9023948063994655330-n.jpg", titulo: "#todoenlaVUE", descripcion: "Haz tu vida más fácil y regístrate en El Guro Emprendedor, el portal gratuito para emprendedores y microempresarios." },
     { url: "https://i.postimg.cc/76myW0Q4/438302983-461813379553979-4329611901029718256-n.jpg", titulo: "Feliz Aniversario", descripcion: "Gracias por apoyar a todos los empresarios y emprendedores del sector de la artesanía y a los artistitas de nuestra región en #ElPaisDeLaBelleza " },
     { url: "https://i.postimg.cc/QtwVy2zh/438301725-7824147357607441-918737923866674437-n.jpg", titulo: "VUE Pamplona", descripcion: "¡Transforma tus sueños empresariales en realidad en tiempo récord con la VUE! 🚀✨¡Tan fácil como suena! 🕒 Ingresa a www.vue.gov.co " },
+    { url: "https://i.postimg.cc/y8t9b5FJ/442372258-7860661773955999-3864680411337824859-n.jpg", titulo: "Buscanos en WhatsApp", descripcion: "Conoce el nuevo canal de difusión que tenemos con toda la actualidad de la Camara de Comercio de Pamplona" },
   ]);
 
   // Cargar imágenes desde localStorage al iniciar
