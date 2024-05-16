f713f3cd46b522fb20433301dfcf8013704fa1e3
archivoccp@gmail.com <archivoccp@gmail.com>
2024-05-16 09:03:39 -0500

    se ajusto la url inicial de la lista de videos en la pagina home, para que estos sean reproducidos de manera infinita

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,7 +6,7 @@ import Baner from "../../componentes/Baner";
 
 const Home = () => {
 
-    const defaultVideoUrl = "https://www.youtube-nocookie.com/embed/videoseries?si=mYpzQKWNUh341Uel&amp;controls=0&amp;list=PLmVJ0CeRYtBxgD2lU-WayzydmZM_K7JgK";
+    const defaultVideoUrl = "https://www.youtube-nocookie.com/embed/videoseries?si=jxE-IkC1qrs1p0cn&amp;controls=0&loop=1&amp;list=PLmVJ0CeRYtBxgD2lU-WayzydmZM_K7JgK";
 
     const [videoUrl, setVideoUrl] = useState('');
 
