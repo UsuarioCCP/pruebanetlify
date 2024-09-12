d2c259267c95bb6c056a8cd70cc9adbbd5999dfe
archivoccp@gmail.com <archivoccp@gmail.com>
2024-09-12 11:10:11 -0500

    cambio de prueba

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,7 +6,9 @@ import Baner from "../../componentes/Baner";
 
 const Home = () => {
 
-    const defaultVideoUrl = "https://www.youtube-nocookie.com/embed/videoseries?si=ddWJhBvp8hQR88nB&amp;controls=0&loop=1&amp;list=PLaZmxwblqOt6M6hgw8u3-h05kt31s5bYM";
+    const defaultVideoUrl = "https://www.youtube.com/watch?v=09m9OgAfko8";
+    
+    // "https://www.youtube-nocookie.com/embed/videoseries?si=ddWJhBvp8hQR88nB&amp;controls=0&loop=1&amp;list=PLaZmxwblqOt6M6hgw8u3-h05kt31s5bYM";
     
 
     const [videoUrl, setVideoUrl] = useState('');
