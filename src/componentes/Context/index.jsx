9a6f84a0db9336068f0d08d7cc555451e5a25198
archivoccp@gmail.com <archivoccp@gmail.com>
2024-11-14 17:54:56 -0500

    ajuste contenido context que se va a mostrar en carousel

--- src/componentes/Context/index.jsx
+++ src/componentes/Context/index.jsx
@@ -8,7 +8,7 @@ export const ImagenesProvider = ({ children }) => {
   // Estado para almacenar las imágenes, inicializado con imágenes predeterminadas
   const [imagenes, setImagenes] = useState([
     { url: "https://i.postimg.cc/kgBDFXGD/441533923-462431496158834-9023948063994655330-n.jpg", titulo: "#todoenlaVUE", descripcion: "Haz tu vida más fácil y regístrate en El Guro Emprendedor, el portal gratuito para emprendedores y microempresarios." },
-    { url: "https://i.postimg.cc/0yQdb0tw/Whats-App-Image-2024-11-14-at-2-12-10-PM.jpg", titulo: "Tenga en cuenta Gaula", descripcion: "🚔👮‍♂️ El Gaula de la Policía te recuerda: ¡Cuelga y bloquea! No caigas en extorsión ⚠️. Reporta al ☎️ 165 gratis. ¡Protege tu seguridad del 👤🕵️!" },
+    { url: "https://i.postimg.cc/0yQdb0tw/Whats-App-Image-2024-11-14-at-2-12-10-PM.jpg", titulo: "Tenga en cuenta", descripcion: "🚔👮‍♂️ El Gaula de la Policía te recuerda: ¡Cuelga y bloquea! No caigas en extorsión ⚠️. Reporta al ☎️ 165 gratis. ¡Protege tu seguridad del 👤🕵️!" },
     { url: "https://i.postimg.cc/4xSvtHMF/Whats-App-Image-2024-11-14-at-2-14-15-PM.jpg", titulo: "Pasarela de la Moda - Pamplona", descripcion: "✨👗 𝙇𝙇𝙀𝙂𝘼 𝙀𝙇 𝙀𝙑𝙀𝙉𝙏𝙊 𝘿𝙀 𝙈𝙊𝘿𝘼 𝙈𝘼́𝙎 𝙀𝙎𝙋𝙀𝙍𝘼𝘿𝙊 ✨ 📅 Viernes, 29 de noviembre📍 Plazuela Almeyda, 5:00 PM 🌟 ¡No te lo pierdas!" },
     { url: "https://i.postimg.cc/y8t9b5FJ/442372258-7860661773955999-3864680411337824859-n.jpg", titulo: "Buscanos en WhatsApp", descripcion: "Conoce el nuevo canal de difusión que tenemos con toda la actualidad de la Camara de Comercio de Pamplona" },
   ]);
