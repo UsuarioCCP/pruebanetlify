5f107fccf9c1f761d343ebd7875b90cd249b7322
archivoccp@gmail.com <archivoccp@gmail.com>
2025-01-15 14:03:06 -0500

    se desactiva el componente snowfall dentro del layout

--- src/componentes/Layout/index.jsx
+++ src/componentes/Layout/index.jsx
@@ -1,10 +1,10 @@
 import './styles.css'
-import { Snowfall } from '../Snowfall/Snowfall';
+// import { Snowfall } from '../Snowfall/Snowfall';
 
 const Layout = ({ children }) => {
     return(
         <div className="flex flex-col">
-            <Snowfall snowflakeCount={200} fallSpeed={20}/>
+            {/* <Snowfall snowflakeCount={200} fallSpeed={20}/> */}
             { children }
         </div>
     )
